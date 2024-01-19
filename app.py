acacb5f0983c418ab05423e417f2972d112cd211
Keitaro Sasaki <sasakeisky21@gmail.com>
2024-01-19 14:47:45 +0900

    revised

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ app.config['UPLOAD_FOLDER'] = './uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000  # 16メガバイト
 app.secret_key = 'your_secret_key_here'
 
+
 kakasi = pykakasi.kakasi()
 
 
@@ -31,13 +32,19 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+class PrintInfo:
+    def __init__(self, papersize, orientation, fitToHeight, fitToWidth):
+        self.papersize = papersize
+        self.orientation = orientation
+        self.fitToWidth = fitToWidth
+        self.fitToHeight = fitToHeight
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
-# db保存用関数
-#index.htmlから/uploadにアクセスがありました
+# ファイルアップロード
+# index.htmlから/uploadにアクセスがありました
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -54,26 +61,28 @@ def upload_file():
             japanese_filename = "".join([item['hepburn'] for item in kakasi.convert(file.filename)])
             filename = secure_filename(japanese_filename)
 
-            #dbに保存
+            #ファイルディレクトリに保存
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             comment1 = 'ファイルが正常に保存されました'
             
-            # return render_template('table.html', header=header, record=record)
             return render_template('index.html', comment=comment1) #index.htmlに返ります
         else:
             raise ValueError('ファイル形式がサポートされていません。')
     return render_template('upload.html') 
 
 
-# index.htmlから/tableにアクセスがありました
-# dbに保存したファイルをtable形式で表示します
+# index.htmlからhrefで/tableにアクセスがありました
+# 保存したファイルをtable形式で表示します
 # 変更があれば上書きします
+# pandasのdataframe形式で読み取ってtable表示
+# jQueryでタブをつまんで列順を並べ変える/またはとってくる列の順番をチェックを入れブラウザで選択する　
+#0,1,2,3,4,にカラム名1,2,3,4が対応
 @app.route('/table') 
 def table():
+    # この中にjQueryか何かでカラムを移動してそれで保存して確定ボタンポチ(POST)でindex.htmlに返ってくる処理
     filepath = os.path.abspath(os.path.dirname) # dbから呼び出し
     file = request.files.get('file')
     filename = file.filename
-    #pandasのdf形式にする
     if filename.endswith('.xlsx') or filename.endswith('.xls') or filename.endswith('.xlsm'):
         df = pd.read_excel(file)
     elif filename.endswith('.csv'):
@@ -82,17 +91,55 @@ def table():
         except:
             df = file.read().decode('shift-jis')
         header = df.columns
-        record = df.values.tolist()
-    return render_template('table.html')
+        record = df.values.tolist() # ここら辺はクラスにした方が使いまわしがよさそう
+
+    # この並びで確定しました(確定ボタンポチ)
+    if request.get('file') == 'POST':
+        # このなかにカラム変更のロジックをかく
+        # htmlでカラムの番号を順番に指定して,functionsの引数でiloc番号を指定してカラムの順番
+        comment2 = '列の順番が変更されました'
+        return render_template('index.html', comment=comment2)
+    return render_template('table.html', header=header, record=record)
+
+
+# index.htmlからhrefで/tableにアクセスがありました
+# アップロードしたファイル全てで
+# ・{指定したコラム(列)}にある氏名で{氏名}が{sheetname}のws
+# または{氏名}で{filename}{sheetname}のbook作る
+@app.route('/make_byname')
+def make_byname():
+    if request.get('file') == 'POST':
+        if request.values == 1:
+            file = function.make_sheet(1) # シートを作るロジックを適用
+        if request.values == 2:
+            file = function.make_sheet(2) # ブックを作るロジックを適用
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        return render_template('index.html')
+    return render_template('make_byname.html')
+
+
+# 氏名スキャンしてws名が{氏名}/book名が{氏名} のものに重複なしでデータを行ごと移植する
+
 
 
 # index.htmlから/printにアクセスがありました
-# dbに保存のあるファイルをExcel形式で呼び出し,printの設定をします
+# ログイン情報で割り当てられたディレクトリにあるファイルをExcel形式で呼び出し,printの設定をします
 @app.route('/print', methods=['GET', 'POST'])
 def print():
     if request.method == 'POST':
-        functions.excel_print()
-    return render_template('index.html')
+        print(request.form) # クラスに適用する引数はsignup.htmlのname属性から取得
+        user_info = PrintInfo(
+            request.form.get('papersize'),
+            request.form.get('orientation'),
+            request.form.get('fitToWidth'),
+            request.form.get('fitToHeight'),
+        ) 
+
+        # functions.excel_print()
+        return render_template('index.html')
+    return render_template('print.html')
+
+
 
 
 def main():
--- functions.py
+++ functions.py
@@ -1,13 +1,81 @@
 import openpyxl
+import pandas as pd
+from glob import glob
+
+from app import PrintInfo
+
+filepath = 'sample.xlsx'
 
 
+# a,b,c,dには列番号を入力してください 表示したい順番に列が並びます
+def arrange(filepath, a, b, c, d):
+  _df = pd.read_excel(filepath)
+  df = _df.iloc[0: , [a+1, b+1, c+1, d+1]]
+  return df
 
-#プリント設定
-def excel_print(filepath):
+
+# 全ファイルで同じ形式で結合
+  df = pd.DataFrame()
+  for filepath in filepaths:
+    _df = arrange(filepath)
+    df = pd.concat([df, _df])
+  df.to_excel('arranged_data.xlsx', index=False)
+
+
+def mkws_byname(filepath):
+  wb = openpyxl.load_workbook(filepath)
+  ws = wb.worksheets[0]
+  namelist = []
+  for i in range(2, ws.max_row+1):
+    name = ws.cell(i, 1).value
+    if name not in namelist:
+      namelist.append(name)
+  for _ in namelist:
+      wb.create_sheet(title=_)
+  return wb
+
+
+# 行ごとnamesheetに移動する
+def get_row(filepath):
+  wb = openpyxl.load_workbook(filepath)
+  ws = wb.active
+
+  row_no = 2
+  for row_no in ws.max_row+1:
+    name = ws.cell(row=row_no, column=1).value # 列1のセルから名前を取得
+    for col in ws.iter_cols():
+      for cell in col:
+        ws[name].cell(row=max_row, column=1).value = cell.value # 同一bookのws[name]の最終行に追加
+    row_no=+1
+  return wb
+
+
+# Paper size(n)selectボタンで設定
+PAPERSIZE_LETTER = '1'
+PAPERSIZE_LETTER_SMALL = '2'
+PAPERSIZE_TABLOID = '3'
+PAPERSIZE_LEDGER = '4'
+PAPERSIZE_LEGAL = '5'
+PAPERSIZE_STATEMENT = '6'
+PAPERSIZE_EXECUTIVE = '7'
+PAPERSIZE_A3 = '8'
+PAPERSIZE_A4 = '9'
+PAPERSIZE_A4_SMALL = '10'
+PAPERSIZE_A5 = '11'
+
+
+printinfo = PrintInfo()
+# クラスからとってきた値を代入する関数にする, 例えば,papersize=10なら10を代入する
+# nに用紙サイズ設定
+def excel_print(filepath, printinfo): #　インスタンスごと渡す
   wb = openpyxl.load_workbook(filepath)
   for ws in wb.worksheets:
-      ws.page_setup.orientation = "portrait" #印刷の向きは縦 横にしたいときは"landscape"
-      ws.page_setup.fitToWidth = 1 #横方向は1ページ(用紙に揃える)
-      ws.page_setup.fitToHeight = 0 #縦方向は用紙に揃えない
-      ws.sheet_properties.pageSetUpPr.fitToPage = True #上2つのオプションを適用する
-  wb.save("setting_print.xlsx") #ファイル名は適宜変更
+      ws.page_setup.orientation =  "f{printinfo.orientation}" #string
+      ws.page_setup.paperSize =  printinfo.papersize #ws.PAPERSIZE_A4
+      ws.page_setup.fitToWidth = printinfo.fitToWidth #0or1
+      ws.page_setup.fitToHeight = printinfo.fitToHeight #0or1
+      ws.sheet_properties.pageSetUpPr.fitToPage = True #True or False
+      
+  return wb
+
+# 今日は,htmlとバックエンドのロジックの連関をやっていきます
